ff5a90c06846c71fb6ae99dca81a8ba9c0c17e60
Louis DeVictoria <telescope40@gmail.com>
2024-06-11 21:55:34 -0400

    remove tests

--- codespaces.sh
+++ codespaces.sh
@@ -1,5 +1,11 @@
 cd netlab/project1
 export ANSIBLE_CONFIG=ansible.cfg
+#mkdir  /usr/local/python/3.10.13/lib/python3.10/site-packages/netsim/validate/bgp/
+#mkdir  /usr/local/python/3.10.13/lib/python3.10/site-packages/netsim/validate/ospf/
+#mkdir  /usr/local/python/3.10.13/lib/python3.10/site-packages/netsim/validate/isis/
+#cp netlab/project1/tests/bgp/frr.py /usr/local/python/3.10.13/lib/python3.10/site-packages/netsim/validate/bgp/frr.py
+#cp netlab/project1/tests/ospf/frr.py /usr/local/python/3.10.13/lib/python3.10/site-packages/netsim/validate/ospf/frr.py
+#cp netlab/project1/tests/isis/frr.py /usr/local/python/3.10.13/lib/python3.10/site-packages/netsim/validate/isis/frr.py
 netlab up
 sleep 120s
 netlab validate
