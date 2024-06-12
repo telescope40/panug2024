a48e7a81384c70035807fe221b10b014de6a7765
Louis DeVictoria <telescope40@gmail.com>
2024-06-12 07:27:14 -0400

    Update CI

--- setup.sh
+++ setup.sh
@@ -21,3 +21,7 @@ sudo tee -a /etc/apt/sources.list.d/netdevops.list -y
 sudo apt update && sudo apt install containerlab -y
 sudo apt-get install containerd -y
 sudo apt remove moby-tini -y
+
+virtualenv netlab/project1
+source netlab/project1/./bin/activate
+netlab install -y ubuntu ansible containerlab
