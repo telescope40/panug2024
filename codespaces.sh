d442c2d485009511ea0d684ebdb9232086c3e890
Louis DeVictoria <telescope40@gmail.com>
2024-06-11 22:21:32 -0400

    add test paths

--- codespaces.sh
+++ codespaces.sh
@@ -1,12 +1,12 @@
 cd netlab/project1
 export ANSIBLE_CONFIG=ansible.cfg
 find /usr -type d -path '*validate*'
-#mkdir  /usr/local/python/3.10.13/lib/python3.10/site-packages/netsim/validate/bgp/
-#mkdir  /usr/local/python/3.10.13/lib/python3.10/site-packages/netsim/validate/ospf/
-#mkdir  /usr/local/python/3.10.13/lib/python3.10/site-packages/netsim/validate/isis/
-#cp netlab/project1/tests/bgp/frr.py /usr/local/python/3.10.13/lib/python3.10/site-packages/netsim/validate/bgp/frr.py
-#cp netlab/project1/tests/ospf/frr.py /usr/local/python/3.10.13/lib/python3.10/site-packages/netsim/validate/ospf/frr.py
-#cp netlab/project1/tests/isis/frr.py /usr/local/python/3.10.13/lib/python3.10/site-packages/netsim/validate/isis/frr.py
+mkdir  /opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/bgp/
+mkdir  /opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/ospf/
+mkdir  /opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/isis/
+cp netlab/project1/tests/bgp/frr.py /opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/bgp/frr.py
+cp netlab/project1/tests/ospf/frr.py /opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/ospf/frr.py
+cp netlab/project1/tests/isis/frr.py /opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/isis/frr.py
 netlab up
 sleep 120s
 netlab validate
