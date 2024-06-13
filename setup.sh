f4d628c8f4aea7ec8a5ecd6087b0280042f29a5f
Louis DeVictoria <telescope40@gmail.com>
2024-06-13 14:46:47 -0400

    Rollback Ubuntu

--- setup.sh
+++ setup.sh
@@ -20,9 +20,9 @@ CWD=$(pwd)
 ##mkdir -p netlab netlab/project1 .devcontainer
 # Phase 2 Install Netlab
 echo "deb [trusted=yes] https://apt.fury.io/netdevops/ /" | \
-sudo tee -a /etc/apt/sources.list.d/netdevops.list -y
-sudo apt update && sudo apt install containerlab -y
-sudo apt-get install containerd -y
+sudo tee -a /etc/apt/sources.list.d/netdevops.list -y | \
+sudo apt update && sudo apt install containerlab -y | \
+sudo apt-get install containerd -y | \
 sudo apt remove moby-tini -y
 
 
