d6efe4d479799f5264666c40ae6f7ed65862ffc8
Louis DeVictoria <telescope40@gmail.com>
2024-06-13 15:26:52 -0400

    Update make

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ CWD=$(pwd)
 # Phase 2 Install Netlab
 echo "deb [trusted=yes] https://apt.fury.io/netdevops/ /" &&\
 sudo tee -a /etc/apt/sources.list.d/netdevops.list -y &&\
-sudo apt update && sudo apt install containerlab -y &&\
+sudo apt install containerlab -y &&\
 sudo apt-get install containerd -y &&\
 sudo apt remove moby-tini -y
 make install
