981c0ebacd10fac5d798db0f444a8d5e539556c0
Louis DeVictoria <telescope40@gmail.com>
2024-06-17 13:53:11 -0400

    Clean up

--- setup.sh
+++ setup.sh
@@ -1,42 +1,29 @@
 #!/usr/bin/env bash
 # Phase 1 Install Tools , Create Directories , Set Up Virtual Environment
 # pip is used to install all my python libraries
-sudo apt-get update && sudo apt-get install -y python3-pip
+#sudo apt-get update && sudo apt-get install -y python3-pip
 # Get current working directory
 
 
 # Phase 2 Install Netlab
-echo "deb [trusted=yes] https://apt.fury.io/netdevops/ /" |\
-sudo tee -a /etc/apt/sources.list.d/netdevops.list -y
-sudo apt install containerlab -y
-sudo apt-get install containerd -y
-sudo apt remove moby-tini -y
-make install
-python3.9 -m venv panug
-source venv/bin/activate
-make install
+echo "deb [trusted=yes] https://apt.fury.io/netdevops/ /" | \
+tee -a /etc/apt/sources.list.d/netdevops.list -y
+apt install containerlab -y
+apt-get install containerd -y
+apt remove moby-tini -y
+apt-get install pip -y
+pip install -r requirements.txt
+#python3.9 -m venv panug
+#source venv/bin/activate
 cd netlab/project1
 netlab install -y ubuntu ansible containerlab
 export ANSIBLE_CONFIG=ansible.cfg
-
-cp -R tests/. ~/.netlab/validate
-PATH=$(find ../../.. -wholename */netsim/validate)
-dirbgp="${PATH}/bgp/"
-dirospf="${PATH}/ospf/"
-dirlinux="${PATH}/linux/"
-dirisis="${PATH}/isis/"
 #Create the Python Virtual Environment
 # Create Directories for the project
-#Copy Test Directories over
-mkdir $dirlinux && mkdir $dirisis && mkdir $dirbgp && mkdir %dirospf
-cp netlab/project1/tests/linux/frr.py $dirlinux
-cp netlab/project1/tests/bgp/frr.py $dirbgp
-cp netlab/project1/tests/ospf/frr.py $dirospf
-cp netlab/project1/tests/isis/frr.py $dirisis
 netlab up
 sleep 20s
 netlab validate
 
 
 
-git remote add netlab git@github.com:ipspace/netlab.git
+#git remote add netlab git@github.com:ipspace/netlab.git
