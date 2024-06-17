8201b2451488ec9cd2b81c1928daabe10a2a2a04
Louis DeVictoria <telescope40@gmail.com>
2024-06-17 14:58:30 -0400

    Update Paths

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,18 +1,5 @@
-#!/bin/bash
-
-# Create the 'telescope' user with a home directory
-useradd -m -s /bin/bash telescope
-
-# Set a password for the 'telescope' user (optional, can be omitted or handled differently)
-echo "telescope:telescope" | chpasswd
-
-# Grant 'telescope' user sudo privileges (optional, remove if not needed)
-echo "telescope ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/telescope
-
-# Make 'telescope' the owner of the /workspace directory
-chown -R telescope:telescope /workspace
-
-# Additional setup for the 'telescope' user can go here
+#!/usr/bin/env bash
+# Install Netlab and Run
 echo "deb [trusted=yes] https://apt.fury.io/netdevops/ /" | \
 tee -a /etc/apt/sources.list.d/netdevops.list -y
 apt install containerlab -y
@@ -20,13 +7,26 @@ apt-get install containerd -y
 apt remove moby-tini -y
 apt-get install pip -y
 pip install -r requirements.txt
+#Add Test Directories and Files
+mkdir -p /usr/local/python/3.9.19/lib/python3.9/site-packages/netsim/validate/isis/
+mkdir -p /usr/local/python/3.9.19/lib/python3.9/site-packages/netsim/validate/ospf/
+mkdir -p /usr/local/python/3.9.19/lib/python3.9/site-packages/netsim/validate/bgp/
+mkdir -p /usr/local/python/3.9.19/lib/python3.9/site-packages/netsim/validate/route/
+cp validation/isis/frr.py /usr/local/python/3.9.19/lib/python3.9/site-packages/netsim/validate/isis/.
+cp validation/ospf/frr.py /usr/local/python/3.9.19/lib/python3.9/site-packages/netsim/validate/ospf/.
+cp validation/bgp/frr.py /usr/local/python/3.9.19/lib/python3.9/site-packages/netsim/validate/bgp/.
+cp validation/route/frr.py /usr/local/python/3.9.19/lib/python3.9/site-packages/netsim/validate/route/.
 #python3.9 -m venv panug
 #source venv/bin/activate
-cd netlab/project1
 netlab install -y ubuntu ansible containerlab
-export ANSIBLE_CONFIG=ansible.cfg
 #Create the Python Virtual Environment
 # Create Directories for the project
+cd netlab
+export ANSIBLE_CONFIG=ansible.cfg
 netlab up
 sleep 20s
 netlab validate
+
+
+
+#git remote add netlab git@github.com:ipspace/netlab.git
--- setup.sh
+++ setup.sh
@@ -7,6 +7,15 @@ apt-get install containerd -y
 apt remove moby-tini -y
 apt-get install pip -y
 pip install -r requirements.txt
+mkdir -p /opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/isis/
+mkdir -p /opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/ospf/
+mkdir -p /opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/bgp/
+mkdir -p/opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/route/
+cp validation/isis/frr.py /opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/isis/.
+cp validation/ospf/frr.py /opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/ospf/.
+cp validation/bgp/frr.py /opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/bgp/.
+cp validation/route/frr.py /opt/hostedtoolcache/Python/3.9.19/x64/lib/python3.9/site-packages/netsim/validate/route/.
+
 #python3.9 -m venv panug
 #source venv/bin/activate
 netlab install -y ubuntu ansible containerlab
