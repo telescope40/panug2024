fb2e060e6aec87a35f501f62f76a74ed290cf8ce
Louis DeVictoria <telescope40@gmail.com>
2024-06-17 14:25:28 -0400

    Add Codespace setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env bash
-# Phase 1 Install Tools , Create Directories , Set Up Virtual Environment
-# pip is used to install all my python libraries
-#sudo apt-get update && sudo apt-get install -y python3-pip
-# Get current working directory
-# Create the 'telescope' user with a home directory
-useradd -m -s /bin/bash telescope
-
-# Set a password for the 'telescope' user (optional, can be omitted or handled differently)
-echo "telescope:telescope" | chpasswd
-
-# Grant 'telescope' user sudo privileges (optional, remove if not needed)
-echo "telescope ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/telescope
-
-# Make 'telescope' the owner of the /workspace directory
-chown -R telescope:telescope /workspace
-
-# Phase 2 Install Netlab
+# Install Netlab and Run
 echo "deb [trusted=yes] https://apt.fury.io/netdevops/ /" | \
 tee -a /etc/apt/sources.list.d/netdevops.list -y
 apt install containerlab -y
