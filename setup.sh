eac4feea50bc81ddd79936409db557b4bc76bbcc
Louis DeVictoria <telescope40@gmail.com>
2024-06-17 14:12:52 -0400

    Add Codespace setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,17 @@
 # pip is used to install all my python libraries
 #sudo apt-get update && sudo apt-get install -y python3-pip
 # Get current working directory
+# Create the 'telescope' user with a home directory
+useradd -m -s /bin/bash telescope
 
+# Set a password for the 'telescope' user (optional, can be omitted or handled differently)
+echo "telescope:telescope" | chpasswd
+
+# Grant 'telescope' user sudo privileges (optional, remove if not needed)
+echo "telescope ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/telescope
+
+# Make 'telescope' the owner of the /workspace directory
+chown -R telescope:telescope /workspace
 
 # Phase 2 Install Netlab
 echo "deb [trusted=yes] https://apt.fury.io/netdevops/ /" | \
@@ -15,11 +25,11 @@ apt-get install pip -y
 pip install -r requirements.txt
 #python3.9 -m venv panug
 #source venv/bin/activate
-cd netlab/project1
 netlab install -y ubuntu ansible containerlab
-export ANSIBLE_CONFIG=ansible.cfg
 #Create the Python Virtual Environment
 # Create Directories for the project
+cd netlab
+export ANSIBLE_CONFIG=ansible.cfg
 netlab up
 sleep 20s
 netlab validate
