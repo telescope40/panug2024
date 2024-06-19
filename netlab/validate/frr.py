a105acdf4bd6ad23e2792e45352c2eeb314905f0
Louis DeVictoria <telescope40@gmail.com>
2024-06-19 07:30:19 -0400

    Add Git Ignore

--- netlab/validate/frr.py
+++ netlab/validate/frr.py
@@ -3,7 +3,7 @@
 # Import FRRouting OSPF and BGP checks and Linux ping/route checks
 #
 from box import Box
-from netsim.validate.ospf.frr import *
-from netsim.validate.isis.frr import *
-from netsim.validate.bgp.frr import *
+from ospf.frr import *
+from isis.frr import *
+from bgp.frr import *
 from netsim.validate.linux import *
